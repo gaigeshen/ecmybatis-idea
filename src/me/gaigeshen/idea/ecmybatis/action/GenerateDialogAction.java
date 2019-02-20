224c719e98b5d5f01dfe4024bebfbb411f5e540e
gaigeshen <gaigeshen@qq.com>
2019-02-20 18:09:47 +0800

    添加菜单用于弹出生成文件对话框

---
+++ src/me/gaigeshen/idea/ecmybatis/action/GenerateDialogAction.java
@@ -0,0 +1,19 @@
+package me.gaigeshen.idea.ecmybatis.action;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+
+
+/**
+ * 点击菜单弹出生成文件对话框
+ *
+ * @author gaigeshen
+ * @since 02/20 2019
+ */
+public class GenerateDialogAction extends AnAction {
+
+  @Override
+  public void actionPerformed(AnActionEvent e) {
+
+  }
+}
